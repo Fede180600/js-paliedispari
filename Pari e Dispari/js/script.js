9e6b623a0459b1e51cc21c18d4cb5939d0023bda
Fede180600 <fede180600@libero.it>
2022-04-21 17:12:40 +0200

    prompts and 1st function done

--- Pari e Dispari/js/script.js
+++ Pari e Dispari/js/script.js
@@ -6,9 +6,23 @@
 
 // Chiedo all'utente di inserire pari o dispari 
 // Chiedo all'utente di inserire un numero da 1 a 5
-
+const userChoice = prompt("Scegli 'pari' o 'dispari'");
+const userNumber = parseInt(prompt("Dimmi un numero da 1 a 5"));
+console.log("L'utente ha scelto:", userChoice);
+console.log("L'utente ha giocato:", userNumber);
 // Genero un numero random (da 1 a 5) per il computer (funzione)
-
+const computerNumb = generateRndInteger(1, 5);
+console.log("Il computer ha giocato:", computerNumb);
+/**
+ * Description
+ * @param {Number} min -> minimum limit (1)
+ * @param {Number} max -> maximum limit (5)
+ * @returns {Number} -> random number between 1 & 5
+ */
+function generateRndInteger(min, max) {
+    const randNumb = Math.floor(Math.random() * (max - min + 1) ) + min;
+    return randNumb;
+}
 // Sommo i due numeri 
 
 // Stabilisco se la somma è pari o dispari (funzione)
